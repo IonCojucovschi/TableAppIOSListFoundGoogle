134e7acf7ab30d529648e6b2646984822f39c760
Cojucovschi Ion <Cojucovschi@gmail.com>
2018-06-18 13:20:02 +0300

    Fist Comit (alll works well).

---
+++ TbleView/TbleView/TableSource.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace TbleView
+{
+    public class TableSource : UITableViewSource
+    {
+
+        protected string[] tableItems;
+        protected string cellIdentifier = "TabvleCell";
+        ViewController owner;
+
+
+        public TableSource(string[] data,ViewController owner)
+        {
+            tableItems = data;
+            this.owner = owner;
+        }
+
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            //request to a cell to save memory 
+            UITableViewCell cell = tableView.DequeueReusableCell(cellIdentifier);
+
+            // if there no cels to reuse, create a new one
+            if (cell == null)
+                cell = new UITableViewCell(UITableViewCellStyle.Default, cellIdentifier);
+
+            cell.TextLabel.Text = tableItems[indexPath.Row];
+            return cell;
+         }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return tableItems.Length;
+        }
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            UIAlertController alertController = UIAlertController.Create("Row Selected", tableItems[indexPath.Row], UIAlertControllerStyle.Alert);
+            alertController.AddAction(UIAlertAction.Create("OK",UIAlertActionStyle.Default,null));
+            owner.PresentViewController(alertController,true,null);
+
+            UIApplication.SharedApplication.OpenUrl(new NSUrl("https:/www.google.com/#q="+tableItems[indexPath.Row]));
+            tableView.DeselectRow(indexPath, true);
+        }
+
+
+
+    }
+}
---
+++ TbleView/TbleView/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using UIKit;
+
+namespace TbleView
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            string[] items = new string[] { "shirts", "pants", "shoes", "Coats" };
+            TableData.Source = new TableSource(items, this);
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
